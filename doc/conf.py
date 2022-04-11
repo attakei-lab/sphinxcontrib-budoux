0701dda880b7d22226a5339f68ee31b989b85e9e
Kazuya Takei <myself@attakei.net>
2022-04-12 00:37:13 +0900

    doc: Register very-minimum doc

---
+++ doc/conf.py
@@ -0,0 +1,21 @@
+# -- Path setup
+
+# -- Project information
+project = "sphinxcontrib-budoux"
+copyright = "2022, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0.0.0"
+language = "ja"
+
+# -- General configuration
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.budoux",
+    "sphinx_rtd_theme",
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
